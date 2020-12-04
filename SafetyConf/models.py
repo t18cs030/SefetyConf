aaaaf4ea8acea3dbf720e9fc703fb73b15ed0362
KeitoFujikawa <t18cs042@yamanashi.ac.jp>
2020-12-04 16:38:39 +0900

    change models.py
    各モデルを追加した

--- SafetyConf/models.py
+++ SafetyConf/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from enum import unique
 
-# Create your models here.
+class Employee(models.Model):
+    employeeId = models.IntegerField(null=False,unique=True)
+    name = models.CharField(max_length=30)
+    mailaddress = models.EmailField()
+    subMailaddress = models.EmailField(null=True,blank=True)
+    group = models.ForeignKey(Group,on_delete=models.CASCADE)
+    
+class Groupe(models.Model):
+    groupId = models.IntegerField(null=False,unique=True)
+    name = models.CharField(max_length=30)
+    
+class EmergencyContact(models.Model):
+    emergencyContactId = models.IntegerField(max_length=10,null=False,unique=True)
+    destinationGroupe = models.ForeignKey(Group,on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    text = models.TextField()
+    deadline = models.DateTimeField()
+    sendDate = models.DateTimeField()
+    
+class Answer(models.Model):
+    employee = models.OneToOneField(Employee,on_delete=models.CASCADE)
+    sendDate = models.DateTimeField()
+    
+    
