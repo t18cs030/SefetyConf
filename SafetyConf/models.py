792c1b417f1b5bccd7102ebd43e3188f9a3fa82b
t18cs017 <t18cs017@lw07.kki.yamanashi.ac.jp>
2020-12-11 15:24:40 +0900

    画面遷移の実装

--- SafetyConf/models.py
+++ SafetyConf/models.py
@@ -14,7 +14,7 @@ class Employee(models.Model):
     
     
 class EmergencyContact(models.Model):
-    emergencyContactId = models.IntegerField(max_length=10,null=False,unique=True)
+    emergencyContactId = models.IntegerField(null=False,unique=True)
     destinationGroupe = models.ForeignKey(Group,on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     text = models.TextField()
--- SafetyConf/views.py
+++ SafetyConf/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.template.backends.django import Template
 
 class IndexView(TemplateView):
-    template_name = "SafetyConf/SafetyConf_index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+    template_name = "SafetyConf/SafetyConf_Index.html"
     
+class AddView(TemplateView):
+    template_name = "SafetyConf/SafetyConf_Add.html"
+    
+class EmergencyListView(TemplateView):
+    template_name = "SafetyConf/SafetyConf_EmergencyList.html"
+    
+class SendView(TemplateView):
+    template_name = "SafetyConf/SafetyConf_Send.html"
+    
+class EmployeeListView(TemplateView):
+    template_name = "SafetyConf/SafetyConf_EmployeeList.html" 
+    
+class TestSendView(TemplateView):
+    template_name = "SafetyConf/SafetyConf_TestSend.html"
+    
+class ResultView(TemplateView):
+    template_name = "SafetyConf/SafetyConf_Result.html"
 # Create your views here.
